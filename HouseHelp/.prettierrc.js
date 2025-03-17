b73641d7b27e2139be030a8891b00ca39e2c1725
divinecia <divineiracia@gmail.com>
2025-03-17 16:19:30 +0200

    new update today

---
+++ HouseHelp/.prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singlemodule,exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+    },
+  Quote: true,
+  printWidth: 120,
+  tabWidth: 2,
+};
